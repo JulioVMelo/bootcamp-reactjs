39630eebefaa3ba71fa3c70c24bef60cc9462c47
Júlio Melo <juliovilar93@gmail.com>
2018-12-04 22:38:04 -0300

    utilizando default props e propTypes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-const soma = (a, b) => a + b;
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { render } from "react-dom";
 
-alert(soma(10, 4));
+class MeuNome extends Component {
+    render() {
+        return <span>Júlio V. Melo</span>
+    }
+}
+
+class Button extends Component {
+    static defaultProps = {
+        children: 'salvar',
+    }
+    
+    static propTypes = {
+        onClick: PropTypes.func.isRequired,
+        children: PropTypes.string
+    }
+
+    render() {
+        return <button onClick={this.props.onClick}>
+            {this.props.children}
+        </button>
+    }
+}
+
+
+
+class App extends Component {
+    handleClick() {
+        alert('botao clicado')
+    }
+    render() {
+        return (
+            <Fragment>
+                <h1>Hello, <MeuNome /> </h1>
+                <Button onClick={this.handleClick}>Enviar children</Button>
+                <Button onClick={() => { alert('button 1') } }/>
+            </Fragment>
+        )
+    }
+}
+
+render(<App />, document.getElementById('app'));
